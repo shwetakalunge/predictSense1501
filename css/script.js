287e540c1782a14b53fef31c563204a5b89bbdde
shwetakalunge <shwetak@WinjitTechnologiesPvtLtd.local>
2021-01-15 14:03:46 +0530

    first-commit

--- css/script.js
+++ css/script.js
@@ -1,7 +1,7 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelector('.nav-links li');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
     //Toggle-Nav
     burger.addEventListener('click', () => {
