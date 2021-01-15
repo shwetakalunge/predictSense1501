9e9a6a0afdba0a2fa5fa2c33358b67917b402194
shwetakalunge <shwetak@WinjitTechnologiesPvtLtd.local>
2021-01-15 14:10:16 +0530

    first-commit

--- css/script.js
+++ css/script.js
@@ -3,15 +3,22 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
-    //Toggle-Nav
-    burger.addEventListener('click', () => {
+
+    burger.addEventListener('click', () => { //Toggle-Nav
         nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+
+            if (link.style.animation) {
+                link.style.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+2}s`
+            }
+        });
     });
 
-    //Animate Links
-    navLinks.forEach((link, index) => {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+2}s`
-    });
+
 }
 
 
